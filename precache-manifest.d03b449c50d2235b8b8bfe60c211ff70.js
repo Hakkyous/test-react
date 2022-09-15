697be2934e092d8674987ffa142b290047e38cdd
Hakkyous <55200383+Hakkyous@users.noreply.github.com>
2022-09-15 01:02:08 -0300

    Updates

---
+++ precache-manifest.d03b449c50d2235b8b8bfe60c211ff70.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d16b2d1ba425b874f70c",
+    "url": "/react-tutorial/static/css/main.f5b3ed12.chunk.css"
+  },
+  {
+    "revision": "d16b2d1ba425b874f70c",
+    "url": "/react-tutorial/static/js/main.d16b2d1b.chunk.js"
+  },
+  {
+    "revision": "f3b84ad1a4a60d245526",
+    "url": "/react-tutorial/static/js/1.f3b84ad1.chunk.js"
+  },
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "6d0de7613bcc52a3d34a43912cf45607",
+    "url": "/react-tutorial/index.html"
+  }
+];
